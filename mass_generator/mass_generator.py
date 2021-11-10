7ef670f902d7971c658db7b48d17fab1d3e0ab28
ch park <qhqhahah@naver.com>
2021-11-10 12:12:30 +0900

    3rd commit

--- mass_generator/mass_generator.py
+++ mass_generator/mass_generator.py
@@ -1,12 +1,13 @@
 ﻿import Rhino.Geometry as rg
 import rhinoscriptsyntax as rs
-import ghpythonlib.components as gh
 import json
+import time
 
 
 class Data:
     def __init__(self, file_path):
         self.file_path = file_path
+        self.data_features = self.read_data()['features']
         
     def read_data(self):
         with open(self.file_path, 'r') as data:
@@ -17,40 +18,85 @@ class Data:
         return self.read_data().keys()
         
     def read_geometry(self):
-        data_features = self.read_data()['features']
+        geometry_data = []
+        for data in self.data_features:
+            geometries = data['geometry']['coordinates'][0][0]
+            geometry_data.append(geometries)
+        return geometry_data
         
-        data_geometries = []
-        for d in data_features:
-            d = d['geometry']['coordinates'][0][0]
-            data_geometries.append(d)
+    def read_information(self):
+        information_data = []
+        for data in self.data_features:
+            properties = data['properties']
+            information_data.append(properties)
+        return information_data
+        
+    def calculate_height(self):
+        building_information = self.read_information()
+        building_heights = []
+        floor_height = 4
+        for information in building_information:
+            height_information = information['HEIGHT']
+            floor_information = information['GRND_FLR']
             
-        return data_geometries
+            building_height = max(height_information, floor_information * floor_height)
+            building_heights.append(building_height)
+        return building_heights
 
 
 class Generator(Data):
     def __init__(self, path):
         Data.__init__(self, path)
-        self.coordinates = self.read_geometry()
         
     def generate_polylines(self):
         polylines = []
-        for coordinates in self.coordinates:
+        coordinates = self.read_geometry()
+        for coords in coordinates:
             points = []
             
-            for c in coordinates:
+            for c in coords:
                 x, y, z = c[0], c[1], 0
                 point = rs.AddPoint(x, y, z)
                 points.append(point)
             
             polyline = rs.AddPolyline(points)
             polylines.append(polyline)
-            
         return polylines
+        
+    def extrude_polylines(self):
+        building_polylines = self.generate_polylines()
+        building_heights = self.calculate_height()
+        
+        buildings = []
+        for i, polyline in enumerate(building_polylines):
+            z = building_heights[i]
+            path = rs.AddLine(rs.AddPoint(0,0,0), rs.AddPoint(0,0,z))
+            building = rs.ExtrudeCurve(polyline, path)
+            
+            if building is not None:
+                rs.CapPlanarHoles(building)
+            
+            buildings.append(building)
+        return buildings
 
 
 
 if __name__ == "__main__":
     data_object = Generator(file_path)
+    print(data_object.read_information()[0])
+    print(data_object.read_information()[1])
     
-    polylines = data_object.generate_polylines()
+    buildings = data_object.extrude_polylines()
     
+    heights = data_object.calculate_height()
+    min_height = min(heights)
+    max_height = max(heights)
+    
+#    structure_code = []
+#    for data in data_object.read_information():
+#        strct_cd = data['STRCT_CD']
+#        if strct_cd is not None:
+#            structure_code.append(data['STRCT_CD'])
+#    
+#    print(sorted(list(set(structure_code))))
+#    print(set(structure_code))
