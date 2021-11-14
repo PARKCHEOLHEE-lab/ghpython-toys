a19168f66557b51b61efe3fbd3a31bb8f9def0be
ch park <qhqhahah@naver.com>
2021-11-14 15:06:55 +0900

    Genetic Algorithm Basic

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -4,22 +4,22 @@ import numpy as np
 from numpy.lib.function_base import select
 
 class GeneticAlgorithm:
-    def __init__(self, gene_count, genome_count, mutation_rate, dominant_factor, generation_limit=1000):
+    def __init__(self, gene_count, chromosome_count, mutation_rate, dominant_factor, generation_limit=1000):
         self.GENE_COUNT = gene_count
-        self.GENOME_COUNT = genome_count
+        self.GENOME_COUNT = chromosome_count
         self.MUTATION_RATE = mutation_rate
         self.GENERATION_LIMIT = generation_limit
         self.DOMINANT_FACTOR = dominant_factor
 
-    def generate_chromosome(self):
-        chromosome = []
+    def generate_genome(self):
+        genome = []
         for _ in range(self.GENOME_COUNT):
-            genome = []
+            chromosome = []
             for _ in range(self.GENE_COUNT):
                 gene = random.randint(0, 9)
-                genome.append(gene)
-            chromosome.append(genome)
-        return chromosome    
+                chromosome.append(gene)
+            genome.append(chromosome)
+        return genome    
 
     def manipulation_chromosome(self, parent_chromosome, child_chromosome, best_parents_indices):
         for i in range(self.GENOME_COUNT):
@@ -33,11 +33,11 @@ class GeneticAlgorithm:
                     crossoverd_gene = parent_chromosome[selected_parent][j]
                     child_chromosome[i][j] = crossoverd_gene
 
-    def evaluate_chromosome(self, chromosome):
-        evaluation_values = [0] * len(chromosome)
-        for i, genome in enumerate(chromosome):
+    def evaluate_chromosome(self, genome):
+        evaluation_values = [0] * len(genome)
+        for i, chromosome in enumerate(genome):
             score = 0
-            for gene in genome:
+            for gene in chromosome:
                 score += abs(gene - self.DOMINANT_FACTOR)
             evaluation_values[i] = score
         return evaluation_values
@@ -46,7 +46,7 @@ class GeneticAlgorithm:
         return copy.deepcopy(chromosome)
 
     def evaluate_dominant(self, evaluation_values):
-        if evaluation_values.count(0) == GENOME_COUNT:
+        if evaluation_values.count(0) == CHROMOSOME_COUNT:
             return True
 
         # for i in range(GENOME_COUNT):
@@ -65,21 +65,21 @@ class GeneticAlgorithm:
         generation = 1
         while generation < self.GENERATION_LIMIT:
             if generation == 1:
-                parent_chromosome = self.generate_chromosome()
-                child_chromosome = self.copy_chromosome(parent_chromosome)
+                parent_genome = self.generate_genome()
+                child_genome = self.copy_chromosome(parent_genome)
 
-            evaluation_values = self.evaluate_chromosome(parent_chromosome)
+            evaluation_values = self.evaluate_chromosome(parent_genome)
             best_parents_indices = self.best_parents(evaluation_values)
 
-            self.manipulation_chromosome(parent_chromosome, child_chromosome, best_parents_indices)
-            parent_chromosome = self.copy_chromosome(child_chromosome)
+            self.manipulation_chromosome(parent_genome, child_genome, best_parents_indices)
+            parent_genome = self.copy_chromosome(child_genome)
 
             if self.evaluate_dominant(evaluation_values):
                 break
 
             print("\n")
             print("generation:", generation)
-            print("chromosome:", parent_chromosome)
+            print("chromosome:", parent_genome)
             print("evaluation value:", evaluation_values)
             print("selected parents:", best_parents_indices)
 
@@ -90,14 +90,14 @@ class GeneticAlgorithm:
 if __name__ == "__main__":
     random.seed(777)
 
-    GENE_COUNT = 10
-    GENOME_COUNT = 5
-    MUTATION_RATE = 0.01
-    DOMINANT_FACTOR = 9
-    GENERATION_LIMIT = 1000
+    GENE_COUNT = 7
+    CHROMOSOME_COUNT = 7
+    MUTATION_RATE = 0.07
+    DOMINANT_FACTOR = 7
+    GENERATION_LIMIT = 777
 
     ga = GeneticAlgorithm(gene_count=GENE_COUNT,
-                          genome_count=GENOME_COUNT,
+                          chromosome_count=CHROMOSOME_COUNT,
                           mutation_rate=MUTATION_RATE,
                           dominant_factor=DOMINANT_FACTOR,
                           generation_limit=GENERATION_LIMIT)
