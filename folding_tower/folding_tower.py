8b812df5061bed257110afe33ec170beef67231e
ch park <qhqhahah@naver.com>
2021-11-29 18:44:50 +0900

    Folding Tower

--- folding_tower/folding_tower.py
+++ folding_tower/folding_tower.py
@@ -131,7 +131,7 @@ class Tower(Rectangle):
                     idx_2 = 0
                 
                 straight = rs.AddLine(curr_points[idx_1], next_points[idx_1])
-                lines.append(straight)
+#                lines.append(straight)
                 
                 if i % 2 == 0:
                     start_point = curr_points[idx_1]
@@ -165,5 +165,45 @@ if __name__ == "__main__":
     tower_obj = Tower(ORIGIN, SIZE, SIZE, VERTICAL_HEIGHTS, frame_angle)
     tower_frame = tower_obj.rotate_frame()
     tower_struc = tower_obj.generate_main_structure()
-#    tower = tower_obj.generate_surface()
-    a = tower_obj.generate_sub_structure()
+    tower = tower_obj.generate_surface()
+    sub_structure = tower_obj.generate_sub_structure()
+    
+    frame_points = []
+    for frame in tower_frame:
+        exploded_frame = rs.ExplodeCurves(frame)
+        temp_list = []
+        
+        for curve in exploded_frame:
+            divide_points = rs.DivideCurve(curve, 5)
+            temp_list.append(divide_points)
+        
+        frame_points.append(temp_list)
+        
+        
+    preprocess = []
+    for column in sub_structure:
+        divide_points = rs.DivideCurve(column, 5)
+        preprocess.append(divide_points)
+    
+    column_points = []
+    for i in range(0, len(preprocess), 4):
+        column_points.append(preprocess[i:i+4])
+    
+    
+    d = []
+    for i in range(4):
+        for j in range(1,5):
+            temp = []
+            for k in range(5):
+                if k == 4:
+                    last_point = frame_points[k][i][j]
+                    temp.append(last_point)
+                else:
+                    point_1 = frame_points[k][i][j]
+                    point_2 = column_points[k][i][j]
+                
+                    temp.extend([point_1, point_2])
+            
+            curve = rs.AddCurve(temp, 1)
+            d.append(curve)
+
