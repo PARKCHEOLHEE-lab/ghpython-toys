5698e1e95126a040d897349e6b8adb0b0c74683d
ch park <qhqhahah@naver.com>
2021-12-19 17:26:51 +0900

    Voxel Tower

--- voxel_tower/voxel_tower.py
+++ voxel_tower/voxel_tower.py
@@ -11,13 +11,25 @@ class Point:
         self.y = y
         self.z = z
         
+    def __getitem__(self, i):
+        return self.get_coord()[i]
+        
     def __repr__(self):
         return "{}".format(self.get_coord())
+        
+    def __add__(self, other_pt):
+        x, y, z = self.get_coord()
+        ox, oy, oz = other_pt.get_coord()
+        return Point(x+ox, y+oy, z+oz)
         
     def __sub__(self, other_pt):
         x, y, z = self.get_coord()
         ox, oy, oz = other_pt.get_coord()
         return Point(x-ox, y-oy, z-oz)
+        
+    def __mul__(self, n):
+        x,y,z = self.get_coord()
+        return Point(x*n, y*n, z*n)
         
     def get_self_point(self):
         x, y, z = self.get_coord()
@@ -39,8 +51,45 @@ class Rectangle:
     def get_pts(self):
         return self.pts
         
+    def get_center_point(self):
+        rec_pts = self.get_pts()
+        zip_pts = zip(*rec_pts)
+        
+        min_x, min_y = min(zip_pts[0]), min(zip_pts[1])
+        max_x, max_y = max(zip_pts[0]), max(zip_pts[1])
+       
+        x = max_x*0.5 + min_x*0.5
+        y = max_y*0.5 + min_y*0.5
+        z = 0
+        
+        return Point(x,y,z)
+        
+    def generate_rotate_pts(self):
+        rec_pts = self.get_pts()
+        degree = 45
+        
+        rotated_rec_pts = []
+        for pt in rec_pts:
+            x, y, z = pt.get_coord()
+            
+            sin45 = math.sin(math.radians(degree))
+            cos45 = math.cos(math.radians(degree))
+            
+            cx, cy, _ = self.get_center_point()
+            
+            rx = cx + (cos45*(x-cx) - sin45*(y-cy))
+            ry = cy + (sin45*(x-cx) + cos45*(y-cy))
+            
+            rp = Point(rx, ry, z)
+            rotated_rec_pts.append(rp)
+        
+        return rotated_rec_pts
+        
     def generate_rectangle(self):
-        return
+        rotated_rec_pts = self.generate_rotate_pts()
+        converted_rec_pts = [pt.generate_point() for pt in rotated_rec_pts]
+        
+        return rg.PolylineCurve(converted_rec_pts)
         
 if __name__ == "__main__":
     p0 = Point(0,0,0)
@@ -49,25 +98,8 @@ if __name__ == "__main__":
     p3 = Point(0,5,0)
     p4 = p0
     
-    rec_pts = [p0, p1, p2, p3, p4]
-    rec = []
-    for pt in rec_pts:
-        rec.append(pt.generate_point())
+    rectangle_pts = [p0, p1, p2, p3, p4]
+    rectangle = Rectangle(rectangle_pts)
     
-    
-    rotated = []
-    for pt in rec_pts:
-        x,y,z = pt.get_coord()
-        
-        sin45 = math.sin(45)
-        cos45 = math.cos(45)
-        
-        rx = cos45*x - sin45*y
-        ry = sin45*x + cos45*y
-        
-        rp = Point(rx, ry, z).generate_point()
-        rotated.append(rp)
-        
-        
-#x' = cos(45°) * x - sin(45°) * y
-#y' = sin(45°) * x + cos(45°) * y
+    rotated_rec = rectangle.generate_rectangle()
+    a = rectangle.generate_rotate_pts()
