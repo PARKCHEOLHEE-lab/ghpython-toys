facddb602127b5f2e1eece51b3feb2bfacb8a43a
ch park <qhqhahah@naver.com>
2021-12-19 15:51:21 +0900

    Voxel Tower

--- voxel_tower/voxel_tower.py
+++ voxel_tower/voxel_tower.py
@@ -11,18 +11,33 @@ class Point:
         self.y = y
         self.z = z
         
+    def __sub__(self, other_pt):
+        x, y, z = self.get_coord()
+        return
+        
+    def get_self_point(self):
+        x, y, z = self.get_coord()
+        self_pt = Point(x, y, z)
+        return self_pt
+        
     def get_coord(self):
         return self.x, self.y, self.z
         
-    def generate_point(self):
-        return rg.Point3d(*self.get_coord())
+    def get_vector(self, other_pt):
+        self_pt = self.get_self_point()
+        return other_pt - self_pt
         
+    def generate_point(self):
+        x, y, z = self.get_coord()
+        return rg.Point3d(x, y, z)
         
         
 class Rectangle:
-    def __init__(self, plane, size):
-        self.palne = plane
-        self.size = size
+    def __init__(self, pts):
+        self.pts = pts
+        
+    def get_pts(self):
+        return self.pts
         
     def generate_rectangle(self):
         return
