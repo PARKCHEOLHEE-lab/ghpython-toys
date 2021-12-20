e8ddc13980a46fbcd92155a07d878101a3af3d45
ch park <qhqhahah@naver.com>
2021-12-21 00:46:43 +0900

    Voxel Tower

--- voxel_tower/voxel_tower.py
+++ voxel_tower/voxel_tower.py
@@ -4,6 +4,7 @@ import Rhino.Geometry as rg
 import rhinoscriptsyntax as rs
 import ghpythonlib.components as gh
 
+
 class Point:
     def __init__(self, x, y, z):
         self.x = x
@@ -43,14 +44,117 @@ class Point:
         ox, oy, _ = other_pt.get_coord()
         
         distance = ((ox-x)**2 + (oy-y)**2) ** 0.5
-        
         return distance
+        
+    def point_in_polygon(self, boundary):
+        if boundary[0].get_coord() != boundary[-1].get_coord():
+            boundary.append(boundary[0])
+        
+        x, y, z = self.get_coord()
+        
+        pt = Point(x, y, z)
+        ept = Point(10000, pt[1], z)
+        ray = Line(pt, ept)
+        
+        count = 0
+        for i in range(len(boundary)-1):
+                p1 = boundary[i]
+                p2 = boundary[i+1]
+                boundary_line = Line(p1, p2)
+                
+                ray_intsc_pt = ray.intersect_point(boundary_line) 
+                if ray_intsc_pt is not None:
+                    count += 1
+                    
+        if count % 2 == 0:
+            return False
+
+        return True
         
     def generate_point(self):
         x, y, z = self.get_coord()
         return rg.Point3d(x, y, z)
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
         
+    def generate_line(self):
+        return rg.Line(self.p1, self.p2)
         
+    def get_p1_coord(self):
+        return self.p1[0], self.p1[1]
+        
+    def get_p2_coord(self):
+        return self.p2[0], self.p2[1]
+        
+    def intersect_point(self, line2):
+        # 두 선분의 교점을 찾는 함수
+        p1 = self.get_p1_coord()
+        p2 = self.get_p2_coord()
+        p3 = line2.get_p1_coord()
+        p4 = line2.get_p2_coord()
+        
+        # 선분1 => (x1,y1) to (x2, y2); 선분2 => (x3,y3) to (x4, y4)
+        x1, y1 = p1
+        x2, y2 = p2
+        x3, y3 = p3
+        x4, y4 = p4
+        z = 0
+        
+        # 완전히 교차하는 경우에만 점을 생성 
+        # ex) 선분AB의 정점이 선분CD의 사이에 있으면 교차하지 않는 것으로 판단
+        denom = (y4-y3)*(x2-x1) - (x4-x3)*(y2-y1)
+        
+        # 선분이 평행하지 않은 경우
+        if denom != 0:
+            ua = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / denom
+            ub = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / denom
+            if (0 < ua < 1) and (0 < ub < 1): # 교차여부 확인
+                ix = x1 + ua * (x2-x1)
+                iy = y1 + ua * (y2-y1)
+                
+                intersect_point = Point(ix, iy, z)
+                return intersect_point
+                
+        # 선분이 평행한 경우
+        elif denom == 0:
+            ua = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3))
+            ub = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3))
+            if (0 < ua < 1) and (0 < ub < 1): # 교차여부 확인
+                ix = x1 + ua * (x2-x1)
+                iy = y1 + ua * (y2-y1)
+                
+                intersect_point = Point(ix, iy, z)
+                return intersect_point
+
+class Polyline:
+    def __init__(self, pts):
+        self.pts = pts
+        
+    def __len__(self):
+        polyline_pts = self.get_polyline_pts()
+        return len(polyline_pts)
+        
+    def __getitem__(self, i):
+        polyline_pts = self.get_polyline_pts()
+        return polyline_pts[i]
+        
+    def __repr__(self):
+        return "{}".format(self.get_polyline_pts())
+        
+    def get_polyline_pts(self):
+        return self.pts
+        
+    def generate_polyline(self):
+        polyline_pts = self.get_polyline_pts()
+        converted_pts = [pt.generate_point() for pt in polyline_pts]
+        
+        return rg.PolylineCurve(converted_pts)
+
+
 class Rectangle:
     def __init__(self, origin, size):
         self.origin = origin
@@ -68,7 +172,6 @@ class Rectangle:
         size = self.get_size()
         
         added_rectangle = Rectangle(origin+vector, size)
-        
         return added_rectangle
         
     def get_origin(self):
@@ -90,14 +193,35 @@ class Rectangle:
         
         return [p0, p1, p2, p3, p4]
         
-    def generate_rectangle(self):
+    def rotate_rectangle(self, origin):
         rectangle_pts = self.get_rectangle_pts()
+        
+        degree = 45
+        sin45 = math.sin(math.radians(degree))
+        cos45 = math.cos(math.radians(degree))
+        
+        rotated_rectangle_pts = []
+        for pt in rectangle_pts:
+            x, y, z = pt.get_coord()
+            
+            origin_x, origin_y, _ = origin.get_coord()
+
+            rx = origin_x + (cos45*(x-origin_x) - sin45*(y-origin_y))
+            ry = origin_y + (sin45*(x-origin_x) + cos45*(y-origin_y))
+            
+            rp = Point(rx, ry, z)
+            rotated_rectangle_pts.append(rp)
+            
+        return Polyline(rotated_rectangle_pts)
+        
+    def generate_rectangle(self, rectangle):
+        rectangle_pts = rectangle.get_rectangle_pts()
         converted_pts = [pt.generate_point() for pt in rectangle_pts]
         
-        return rg.PolylineCurve(converted_pts)
+        return Polyline(converted_pts).generate_polyline()
 
 
-class Zigzag(Rectangle):
+class Pattern(Rectangle):
     def __init__(self, boundary, size):
         self.boundary = boundary
         
@@ -148,55 +272,98 @@ class Zigzag(Rectangle):
         
         return width, height
         
+    def get_scaled_bbox_cpt(self):
+        _, height = self.get_scaled_bbox_size()
+        scaled_bbox = self.get_scaled_bbox()
+        
+        O = 0
+        anchor = scaled_bbox[0]*0.5 + scaled_bbox[3]*0.5
+        vector = Point(O, height/2, O)
+        
+        center_point = anchor + vector
+        x, y, z = center_point
+        return Point(x, y, z)
+        
     def generate_pattern(self):
         origin = self.get_pattern_origin()
-        base_rec_pts = self.get_rectangle_pts()
-        base_rec_polyline = self.generate_rectangle()
         
         size = self.get_size()
         width, height = self.get_scaled_bbox_size()
         
         height_count = int(height / size) * 2
         width_count = int(width / size)
+        O = 0
         
-        rec_polyline = []
-        rec_pts = []
+        base_pattern = []
+        all_patterns = []
         
         for i in range(height_count):
             curr_origin = origin
             
             if i == 0:
                 copied_rec = Rectangle(curr_origin, size)
-                rec_polyline.append(copied_rec.generate_rectangle())
-                rec_pts.append(copied_rec)
+                base_pattern.append(copied_rec)
                 
-                origin = rec_pts[-1][2]*0.5 + rec_pts[-1][3]*0.5
+                all_patterns.append(copied_rec)
+                origin = base_pattern[-1][2]*0.5 + base_pattern[-1][3]*0.5
             
             elif i % 2 != 0:
                 copied_rec = Rectangle(curr_origin, size)
-                rec_polyline.append(copied_rec.generate_rectangle())                
-                rec_pts.append(copied_rec)
+                base_pattern.append(copied_rec)
                 
-                origin = rec_pts[-2][1]
+                all_patterns.append(copied_rec)
+                origin = base_pattern[-2][1]
                 
             else:
                 copied_rec = Rectangle(curr_origin, size)
-                rec_polyline.append(copied_rec.generate_rectangle())                
-                rec_pts.append(copied_rec)
+                base_pattern.append(copied_rec)
                 
-                origin = rec_pts[-1][2]*0.5 + rec_pts[-1][3]*0.5
+                all_patterns.append(copied_rec)
+                origin = base_pattern[-1][2]*0.5 + base_pattern[-1][3]*0.5
                 
-            
             for j in range(2, width_count, 2):
-                vector = Point(size*j, 0, 0)
-                rec_polyline.append((copied_rec + vector).generate_rectangle())
+                vector = Point(size*j, O, O)
+                copied_pattern = copied_rec + vector
+                
+                all_patterns.append(copied_pattern)
+                
+        return all_patterns
+        
+    def rotate_pattern(self):
+        rotate_origin = self.get_scaled_bbox_cpt()
+        all_patterns = self.generate_pattern()
+        
+        rotate_patterns = []
+        for pattern in all_patterns:
+            rotate_pattern_pts = pattern.rotate_rectangle(rotate_origin)
+            rotate_pattern_polyline = Polyline(rotate_pattern_pts)
             
-        return rec_polyline
+            rotate_patterns.append(rotate_pattern_polyline)
+        
+        return rotate_patterns
+        
+    def cull_pattern(self):
+        rotate_patterns = self.rotate_pattern()
+        boundary = self.get_boundary_pts()
+        
+        for pt in rotate_patterns[667][:-1]:
+            print(pt.point_in_polygon(boundary))
+        
+#        for pattern in rotate_patterns:
+#            for pt in pattern[:-1]:
+#                print(pt.point_in_polygon(boundary))
+#                
+#            break
+        
+        return [p.generate_polyline() for p in rotate_patterns]
+
 
 if __name__ == "__main__":
     
-    ############## Generate Pattern ##############
+    ############## Variable Definition ##############
     pattern_size = 3
-    base_rec_pts = Zigzag(boundary_points, pattern_size)    
+    base_rec_pts = Pattern(boundary_points, pattern_size)  
     
-    pattern = base_rec_pts.generate_pattern()
+    
+    ############## Generate Pattern ##############  
+    pattern = base_rec_pts.cull_pattern()
