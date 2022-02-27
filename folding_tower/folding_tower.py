ab4a43da85295fcec49972a0e05389a7452d7ca4
PARKCHEOLHEE-lab <83874157+PARKCHEOLHEE-lab@users.noreply.github.com>
2022-02-27 15:16:10 +0900

    Add files via upload

--- folding_tower/folding_tower.py
+++ folding_tower/folding_tower.py
@@ -35,14 +35,14 @@ class Rectangle:
 
 
 class Tower(Rectangle):
-    def __init__(self, origin, width, height, vertical_heights, frame_angle):
+    def __init__(self, origin, width, height, frame_heights, frame_angle):
         self.floor_height = 4
-        self.vertical_heights = vertical_heights
+        self.frame_heights = [0] + frame_heights
         self.frame_angle = frame_angle
         Rectangle.__init__(self, origin, width, height)
         
-    def get_vertical_heights(self):
-        return self.vertical_heights
+    def get_frame_heights(self):
+        return self.frame_heights
         
     def get_floor_height(self):
         return self.floor_height
@@ -51,13 +51,13 @@ class Tower(Rectangle):
         return self.frame_angle
         
     def frame_count(self):
-        return len(self.vertical_heights)
+        return len(self.frame_heights)
         
     def generate_frame(self):
         recs = []
         floor_height = self.get_floor_height()
         for i in range(self.frame_count()):
-            z = self.get_vertical_heights()[i]
+            z = self.get_frame_heights()[i]
             copied_rectangle = self.copy_rectangle([0, 0, z*floor_height])
             recs.append(copied_rectangle)
         return recs
@@ -73,6 +73,12 @@ class Tower(Rectangle):
         frame[-1] = roof_frame
         return frame
         
+    def get_frist_floor_length(self):
+        first_floor_curve = self.rotate_frame()[0]
+        first_floor_length = rs.CurveLength(first_floor_curve)
+        
+        return first_floor_length
+        
     def lean_roof(self, frame):
         frame_points = rs.CurvePoints(frame[-1])
         
@@ -82,18 +88,37 @@ class Tower(Rectangle):
         roof_frame = rs.AddCurve(frame_points, degree=1)
         return roof_frame
         
-    def generate_core(self):
-        core_size = -10
-        base_curve = self.rotate_frame()[0]
-        core_curve = rs.OffsetCurve(base_curve, [0,0,0], core_size)
+    def generate_closed_facade(self):
+        merged_facade = self.generate_facade()
+        closed_facade = gh.CapHoles(merged_facade)
         
-        core_z = 160
-        core_path = rs.AddLine(rs.AddPoint(0,0,0), rs.AddPoint(0,0,core_z))
-        core = rs.ExtrudeCurve(core_curve, core_path)
+        return closed_facade
+        
+    def generate_bbox(self):
+        closed_facade = self.generate_closed_facade()
+        
+        tower_bbox = gh.BoundingBox(closed_facade, rg.Plane.WorldXY).box
+        tower_bbox_centroid = gh.Volume(tower_bbox).centroid
+        
+        return tower_bbox, tower_bbox_centroid
+        
+    def generate_core(self):
+        before_rotate_frame = self.generate_frame()
+        first_floor_frame = before_rotate_frame[0]
+        
+        coefficient = 0.3
+        core_origin = gh.Area(rs.coercecurve(first_floor_frame)).centroid
+        core_base = rs.AddLoftSrf(before_rotate_frame)
+        
+        core = gh.ScaleNU(rs.coercebrep(core_base), core_origin, coefficient, coefficient, 1.02).geometry
+        core = gh.CapHoles(core)
+        
+        rs.MoveObject(core, [200, 0, 0])
+        
         return core
         
     def generate_main_structure(self):
-        main_structure = []
+        main_structure_for_facade = []
         frame = self.rotate_frame()
         for i in range(len(frame)-1):
             curr_points = self.get_points(frame[i])[:-1]
@@ -112,7 +137,7 @@ class Tower(Rectangle):
                     polyline_1 = gh.PolyLine(points_1, True)
                     polyline_2 = gh.PolyLine(points_2, True)
                     
-                    main_structure.extend([polyline_1, polyline_2])
+                    main_structure_for_facade.extend([polyline_1, polyline_2])
                     
                 else:
                     points_1 = [curr_points[idx_1], next_points[idx_1], curr_points[idx_2]]
@@ -121,9 +146,14 @@ class Tower(Rectangle):
                     polyline_1 = gh.PolyLine(points_1, True)
                     polyline_2 = gh.PolyLine(points_2, True)
                     
-                    main_structure.extend([polyline_1, polyline_2])
+                    main_structure_for_facade.extend([polyline_1, polyline_2])
                     
-        return main_structure
+        exploded_main_structure_for_facade = gh.Explode(main_structure_for_facade, True).segments
+        main_structure = gh.Pipe(exploded_main_structure_for_facade, 0.25, 2)
+        
+        rs.MoveObjects(main_structure, [105, 0, 0])
+                    
+        return main_structure_for_facade, main_structure
         
     def generate_sub_structure(self):
         tower_frame = self.rotate_frame()
@@ -173,7 +203,8 @@ class Tower(Rectangle):
         for i in range(0, len(preprocess), side_count):
             column_points.append(preprocess[i:i+side_count])
             
-        sub_structure = []
+        sub_structures = []
+        structure_size = rg.Interval(-0.15, 0.15)
         for i in range(4):
             for j in range(1,5):
                 temp = []
@@ -188,33 +219,67 @@ class Tower(Rectangle):
                         temp.extend([point_1, point_2])
                 
                 curve = rs.AddCurve(temp, 1)
-                sub_structure.append(curve)
-            
-                    
-        return sub_structure
+                curve_geom = rs.coercecurve(curve)
+                
+                sub_structure_start_pt = gh.EndPoints(curve_geom).start
+                sub_structure_rec = gh.Rectangle(sub_structure_start_pt, structure_size, structure_size, 0).rectangle
+                
+                sub_structure = gh.Sweep1(curve_geom, sub_structure_rec, 1)
+                sub_structure = gh.Move(sub_structure, rg.Point3d(105, 0, 0)).geometry
+                
+                if isinstance(sub_structure, list):
+                    sub_structures.append(sub_structure[0])
+                    sub_structures.append(sub_structure[1])
+                
+                sub_structures.append(sub_structure)
+        
+        return sub_structures
         
     def generate_facade(self):
-        srfs = []
-        main_structure = self.generate_main_structure()
-        for polyline in main_structure:
+        main_structure_for_facade, _ = self.generate_main_structure()
+        
+        facade = []
+        for polyline in main_structure_for_facade:
             srf = gh.BoundarySurfaces(polyline)
-            srfs.append(srf)
+            facade.append(srf)
+            
+        merged_facade = gh.BrepJoin(facade).breps
+            
+        return merged_facade
         
-#        facade = 
+    def generate_double_skin(self):
+        merged_facade = self.generate_facade()
         
-        return gh.BrepJoin(srfs)[0]
+        tower_bbox, tower_bbox_centroid = self.generate_bbox()
+        first_floor_length = self.get_frist_floor_length()
+        
+        offset_dis = 2
+        coefficient = (first_floor_length/4 + offset_dis) / (first_floor_length/4)
+        
+        double_skin = gh.ScaleNU(merged_facade, tower_bbox_centroid, coefficient, coefficient, 1.02).geometry
+        
+        return double_skin
+        
+    def generate_tower_cap(self):
+        double_skin = self.generate_double_skin()
+        closed_facade = gh.CapHoles(double_skin)
+        bottom_cap = gh.DeconstructBrep(closed_facade).faces[0]
+        upper_cap = gh.DeconstructBrep(closed_facade).faces[1]
+        
+        return bottom_cap, upper_cap
 
 
 if __name__ == "__main__":
-    VERTICAL_HEIGHTS = [0, 5, 16, 30, 40]
     ORIGIN = [200, 0, 0]
     SIZE = 32
     
-    tower_obj = Tower(ORIGIN, SIZE, SIZE, VERTICAL_HEIGHTS, frame_angle)
+    tower = Tower(ORIGIN, SIZE, SIZE, frame_heights, frame_angle)
     
-    tower_facade = tower_obj.generate_facade()
-    main_structure = tower_obj.generate_main_structure()
-    sub_structure = tower_obj.generate_sub_structure()
+    tower_facade = tower.generate_facade()
+    tower_double_skin = tower.generate_double_skin()
+    tower_cap = tower.generate_tower_cap()
+    tower_core = tower.generate_core()
     
-    
-    a = tower_obj.generate_core()
+    _, main_structure = tower.generate_main_structure()
+    sub_structure = tower.generate_sub_structure()
+
---
+++ folding_tower/folding_tower_2.py
@@ -0,0 +1,13 @@
+﻿import math
+import random
+import Rhino.Geometry as rg
+import Rhino.RhinoDoc as rc
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as gh
+
+contours = gh.BoundarySurfaces(contours)
+rs.MoveObject(contours, [200,0,0])
+
+collision = gh.CollisionOneXMany(contours, core).collision
+
+contours = [c for i, c in enumerate(contours) if collision[i] == True]
